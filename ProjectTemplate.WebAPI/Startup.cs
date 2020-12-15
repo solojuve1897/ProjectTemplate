b5012cdb7de2480a97f9d0b5be64cf55abb6b7f2
Mohammad Khalil <mohammad@insuremytrans.com>
2020-12-15 15:24:46 +0100

    ability to set token in Swagger UI

--- ProjectTemplate.WebAPI/Startup.cs
+++ ProjectTemplate.WebAPI/Startup.cs
@@ -11,6 +11,7 @@ using ProjectTemplate.Application.Common.Interfaces;
 using ProjectTemplate.Infrastructure;
 using ProjectTemplate.WebAPI.Filters;
 using ProjectTemplate.WebAPI.Services;
+using System;
 
 namespace ProjectTemplate.WebAPI
 {
@@ -66,6 +67,29 @@ namespace ProjectTemplate.WebAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjectTemplate.WebAPI", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
         }
 
